fb916747f55fa08384642c5eb3d26d67b54d96e7
quizyaka <bodea057@gmail.com>
2023-07-29 23:37:45 +0300

    added french lang

--- lua/menu/!sbox/html/js/menu/control.Menu.js
+++ lua/menu/!sbox/html/js/menu/control.Menu.js
@@ -82,7 +82,8 @@ function MenuController( $scope, $rootScope )
 
 	$scope.SupportLang = {
 		"ru":"img/sbox/flags/ru.png",
-		"en":"img/sbox/flags/us.png"
+		"en":"img/sbox/flags/us.png",
+		'fr':"img/sbox/flags/fr.png"
 	}
 
 	$scope.ChangeLang = function(lang)
